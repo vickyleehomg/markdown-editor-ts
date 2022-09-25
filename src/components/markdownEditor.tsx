8f13813ee28c2afcff427f1bbf096426f964d0c1
Ruiqi Gao <vicky@Ruiqis-MacBook-Pro.local>
2022-09-25 00:26:19 -0600

    codemirror added

--- src/components/markdownEditor.tsx
+++ src/components/markdownEditor.tsx
@@ -1,4 +1,31 @@
 import React, { useContext } from 'react';
+import '../App.css';
+import {
+  Heading,
+  Box,
+  Divider
+} from '@chakra-ui/react';
+import CodeMirror from '@uiw/react-codemirror';
 
+function MarkdownEditor(){
 
-export {};
+  return(
+    <Box>
+      <Heading size='lg'>Markdown Text</Heading>
+        <Divider/>
+        <Box>
+      <CodeMirror
+        value="Hi"
+        height='calc(100vh - 132px)'
+        basicSetup={{
+          lineNumbers:false,
+          foldGutter:false
+        }}
+      />
+      </Box>
+    </Box>
+
+  );
+};
+
+export default MarkdownEditor;
--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,16 @@ import {
     Box,
     Divider
   } from '@chakra-ui/react';
+import MarkdownEditor from './components/markdownEditor';
 
 function Main(){
     return(
         <Flex pos='fixed' mt ='96px'w='100%' h='calc(100vh)-96x'>
-            <Box w='50%' p={4}>
-                Box1
+            <Box w='50%'>
+                <MarkdownEditor/>
             </Box>
             <Divider orientation='vertical'/>
-            <Box w='50%' p={4}>
+            <Box w='50%'>
                 Box2
             </Box>
         </Flex>
