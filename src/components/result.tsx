4bc1cdd85a5af1190639c7e5e543d249e04cf10f
Ruiqi Gao <vicky@Ruiqis-MacBook-Pro.local>
2022-10-01 00:10:56 -0600

    title changed, result.tsx added, better layouts

---
+++ src/components/result.tsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import '../App.css';
+import {
+  Heading,
+  Box,
+  Divider
+} from '@chakra-ui/react';
+
+
+function Result(){
+
+
+  return(
+    <Box ml='8%' mr='8%' my='30px' bg='white' minH='calc(100vh)'>
+
+    </Box>
+
+  );
+};
+
+export default Result;
--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,17 @@ import {
     Divider
   } from '@chakra-ui/react';
 import MarkdownEditor from './components/markdownEditor';
+import Result from './components/result';
 
 function Main(){
     return(
-        <Flex pos='fixed' mt ='96px'w='100%' h='calc(100vh)-96x'>
-            <Box w='50%'>
+        <Flex pos='fixed' mt ='60px'w='100%' h='calc(100vh)-60x'>
+            <Box w='35%'>
                 <MarkdownEditor/>
             </Box>
             <Divider orientation='vertical'/>
-            <Box w='50%'>
-                Box2
+            <Box w='65%' bg='#929292' height={'calc(100vh - 60px)'} overflow={'scroll'}>
+                <Result/>
             </Box>
         </Flex>
    );
