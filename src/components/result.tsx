7b89a0771bf82d7fc9ede726a59e67d79b4f346e
Ruiqi Gao <vicky@Ruiqis-MacBook-Pro.local>
2022-10-01 00:20:02 -0600

    markdown-it installed, rendering html OK

--- src/components/result.tsx
+++ src/components/result.tsx
@@ -5,13 +5,21 @@ import {
   Box,
   Divider
 } from '@chakra-ui/react';
+import MarkdownIt from 'markdown-it';
+
+const markdownParserResume = new MarkdownIt({
+  html: true,
+  breaks: true,
+});
 
 
 function Result(){
 
-
   return(
     <Box ml='8%' mr='8%' my='30px' bg='white' minH='calc(100vh)'>
+        <Box
+            className='rs-view'
+            dangerouslySetInnerHTML={{__html: markdownParserResume.render('# Hello')}}/>
 
     </Box>
 
