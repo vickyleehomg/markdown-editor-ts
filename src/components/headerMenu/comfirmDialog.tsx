46ea9092c2a7de18eaff068f826071854dd4d892
Ruiqi Gao <vicky@Ruiqis-MacBook-Pro.local>
2022-10-03 22:17:31 -0600

    theme select fixed

--- src/components/headerMenu/comfirmDialog.tsx
+++ src/components/headerMenu/comfirmDialog.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext }from 'react';
 import {
     Button,
     AlertDialog,
@@ -6,41 +6,48 @@ import {
     AlertDialogFooter,
     AlertDialogHeader,
     AlertDialogContent,
-    AlertDialogOverlay,
+    AlertDialogOverlay
 } from '@chakra-ui/react'
+import { ThemeContext } from '../../utils/contexts';
+
 
 export default function ConfirmDialog(props:any) {
-    const cancelRef = React.useRef(null)
-  
-    return (  
-        <AlertDialog
-          isOpen={props.isOpen}
-          leastDestructiveRef={cancelRef}
-          onClose={props.onClose}
-        >
-          <AlertDialogOverlay>
-            <AlertDialogContent>
-              <AlertDialogHeader
-                fontSize='lg' fontWeight='bold'
-                bg='blue.50' borderRadius='5px 5px 0 0'>
-                Select Theme
-              </AlertDialogHeader>
-  
-              <AlertDialogBody>
-                Are you sure to use {props.theme} theme?
-              </AlertDialogBody>
-  
-              <AlertDialogFooter>
-                <Button ref={cancelRef} onClick={props.onClose}>
-                  Cancel
-                </Button>
-                <Button colorScheme='blue' onClick={props.onClose} ml={3}>
-                  Confirm
-                </Button>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialogOverlay>
-        </AlertDialog>
-    )
+  const { setTheme } = useContext(ThemeContext);
+  const cancelRef = React.useRef(null)
+
+  return (  
+    <AlertDialog
+      isOpen={props.isOpen}
+      leastDestructiveRef={cancelRef}
+      onClose={props.onClose}
+    >
+      <AlertDialogOverlay>
+        <AlertDialogContent>
+          <AlertDialogHeader
+            fontSize='lg' fontWeight='bold'
+            bg='blue.50' borderRadius='5px 5px 0 0'>
+            Select Theme
+          </AlertDialogHeader>
+
+          <AlertDialogBody>
+            Are you sure to use {props.themeSelect} theme?
+          </AlertDialogBody>
+
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={props.onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='blue' onClick={()=>{
+              props.onClose();
+              setTheme(props.themeSelect);
+              }}
+              ml={3}>
+              Confirm
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  )
 };
   
--- src/components/headerMenu/themes.tsx
+++ src/components/headerMenu/themes.tsx
@@ -1,18 +1,24 @@
-import React, { useContext }from 'react';
+import React, { useState, useContext }from 'react';
 import {
+  Box,
   Button,
   Drawer, DrawerBody, DrawerFooter, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton,
-  useDisclosure,
+  Stack,
+  Text,
+  useDisclosure
 } from '@chakra-ui/react';
 import ConfirmDialog from './comfirmDialog';
-import ThemeCollection from './themeCollection';
 import { ThemeContext } from '../../utils/contexts';
 
 function Themes(){
+    const selected = 'outline';
+    const unSelected = 'md';
+
     const drawer = useDisclosure();
     const dialog = useDisclosure();
 
     const { theme } = useContext(ThemeContext);
+    const [ themeSelect, setThemeSelect ] = useState(theme);
 
     return(
         <>
@@ -23,7 +29,8 @@ function Themes(){
             <Drawer
                 isOpen={drawer.isOpen}
                 placement='right'
-                onClose={drawer.onClose}
+                onClose={()=>{drawer.onClose()
+                            setThemeSelect(theme)}}
             >
                 <DrawerOverlay />
                 <DrawerContent>
@@ -34,7 +41,45 @@ function Themes(){
                     </DrawerHeader>
                     <DrawerCloseButton />
                     <DrawerBody p='20px' overflow='scroll'>
-                        <ThemeCollection/>
+                        <Stack>
+                            <Stack as={Button} variant='link'
+                                onClick={()=>{
+                                    setThemeSelect('warm')}}>
+                                <Box
+                                    w='160px'
+                                    h='200px'
+                                    bgGradient='linear(red.100 0%, orange.100 25%, yellow.100 50%)'
+                                    boxShadow={(drawer.isOpen === true) ? 
+                                        (themeSelect === 'warm') ? selected : unSelected :
+                                        (theme ==='warm') ? selected : unSelected} 
+                                    />
+                                <Text color='blackAlpha.500'>Warm</Text>
+                            </Stack>
+                            
+                            <Stack as={Button} variant='link' onClick={()=>{setThemeSelect('cool')}}>
+                                <Box
+                                    w='160px'
+                                    h='200px'
+                                    bgGradient='linear(blue.100 0%, cyan.100 25%, white 50%, cyan.50 75%)'
+                                    boxShadow={(drawer.isOpen === true) ? 
+                                        (themeSelect === 'cool') ? selected : unSelected :
+                                        (theme ==='cool') ? selected : unSelected}
+                                    />
+                                <Text color='blackAlpha.500'>Cool</Text>
+                            </Stack>
+                            
+                            <Stack as={Button} variant='link' onClick={()=>{setThemeSelect('forest')}}>
+                                <Box
+                                    w='160px'
+                                    h='200px'
+                                    bgGradient='linear(teal.100 0%, teal.50 25%, green.50 50%, green.200 75%)'
+                                    boxShadow={(drawer.isOpen === true) ? 
+                                        (themeSelect === 'forest') ? selected : unSelected :
+                                        (theme ==='forest') ? selected : unSelected}
+                                    />
+                                <Text color='blackAlpha.500'>Forest</Text>
+                            </Stack>
+                        </Stack>
                     </DrawerBody>
                     <DrawerFooter>
                         <Button variant='outline' mr={3} onClick={drawer.onClose}>
@@ -43,7 +88,10 @@ function Themes(){
                         <Button colorScheme='blue' onClick={dialog.onOpen}>
                             Select
                         </Button>
-                        <ConfirmDialog isOpen={dialog.isOpen} onClose={dialog.onClose} theme={theme}/>
+                        <ConfirmDialog
+                            isOpen={dialog.isOpen}
+                            onClose={dialog.onClose}
+                            themeSelect={themeSelect}/>
                     </DrawerFooter>
                 </DrawerContent>
             </Drawer>
