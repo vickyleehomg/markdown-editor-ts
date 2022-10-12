0605a40973a3e2d8638e9d43d936d6af2e71d614
Ruiqi Gao <vicky@Ruiqis-MacBook-Pro.local>
2022-10-11 23:41:56 -0600

    better layout on mobile

--- src/startMobile.tsx
+++ src/startMobile.tsx
@@ -19,27 +19,28 @@ function StartMobile(){
     <Center pos='fixed' w='100%' h='100%'>
       <Box w='100%'>
         <Center>
-            <VStack align='left'  >
-            <Heading size='3xl' >MARKDOWN</Heading>
-            <Heading size='3xl' >RESUME</Heading>
-            <Heading size='sm' lineHeight='tall'>
-                <Highlight
-                    query='content'
-                    styles={{ px: '1', rounded: 'full', bg: 'teal.200' }}
-                >
-                Focus on the content, no more painful layouts.
-                </Highlight>
-            </Heading>
+            <VStack  >
+              <Heading size='3xl' >MARKDOWN</Heading>
+              <Heading size='3xl' >RESUME</Heading>
             </VStack>
         </Center>
         
         <Center to='/editor' as={Link}  bg='blue.50' minH='80px'>
           <Heading size='2xl'>
-          Get Start <Icon as={BiRightArrowCircle}/>
+            Get Start <Icon as={BiRightArrowCircle}/>
+          </Heading>
+        </Center>
+        <Center>
+          <Heading size='sm' lineHeight='tall'>
+              <Highlight
+                  query='content'
+                  styles={{ px: '1', rounded: 'full', bg: 'teal.200' }}
+              >
+              Focus on the content, no more painful layouts.
+              </Highlight>
           </Heading>
         </Center>
       </Box>
-
     </Center>
   );
 };
